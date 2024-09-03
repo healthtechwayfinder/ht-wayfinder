9c54079d98b2ae4bf3224889bd42c8393f840e9d
swamplet <bridget.slomka@gmail.com>
2024-09-03 02:28:27 -0400

    Update and rename 1_test1.py to Need_Statement_Logger.py
    added process details

---
+++ pages/Need_Statement_Logger.py
@@ -0,0 +1,26 @@
+# test page
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+import pandas as pd
+
+
+# //// PROCESS ////
+# 0. brief instructions appear at the top of the page with a link to the uder guide for more info
+# 1. select one or more observations (type them in? drop down?)
+#     -> summaries of observations are then displayed
+# 2. select the date (default to today's date)
+# 3. select the author (or NOT???? --  let's think this over)
+# 4. enter statement:
+#      -> 1st box: enter problem
+#      -> 2nd box: enter population
+#      -> 3rd box: enter outcome
+#      -> 4th box: enter full need statement
+#      -> 5th box for notes?
+#    -> statement goes to sheet and information is recorded in corresonding columns
+# 5. option to enter more statements with a (+) button (with a unique ID for each statement, user doesn't need to see this, honestly)
+# 6. statement goes to the google sheet, no AI necessary -- user sees message "Need statement(s) recorded!)
+# Other Notes:
+# -> code could lay foundation for detecting and sorting problem, population, and solution rather than manual entry
+# -> could the observation bot page have a widget in the right-hand sidebar for entering need satements from that page? (in case something comes up from a conversation)
+
+# I propose copying the code over form the Add Observation page, but removing the AI components -- only entering info from the user right to the log
