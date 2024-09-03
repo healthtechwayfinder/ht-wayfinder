63c6d3d6f33132a167817dfdcc6464b0f8a64c40
swamplet <bridget.slomka@gmail.com>
2024-09-03 02:40:15 -0400

    Update 1_User_Guide.py

--- pages/1_User_Guide.py
+++ pages/1_User_Guide.py
@@ -3,8 +3,31 @@ from streamlit_extras.switch_page_button import switch_page
 
 
 def main():
-    st.markdown("<h1 style='text-align: center;'>User Guide</h1>", unsafe_allow_html=True)
-    st.markdown("<h2 style='text-align: center;'>Adding an Observation</h2>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;'>User Guide: Observations</h1>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center;'>Add an Observation</h3>", unsafe_allow_html=True)
+    # add in a description of this feature
+    
+    st.markdown("<h3 style='text-align: center;'>Chat with Observations</h3>", unsafe_allow_html=True)
+    # add in a description of this feature
+
+    st.markdown("<h3 style='text-align: center;'>View Observations</h3>", unsafe_allow_html=True)
+    # add in a description of this feature
+    
+    st.markdown("<h3 style='text-align: center;'>Glossary</h3>", unsafe_allow_html=True)
+    # add in a description of this feature
+
+    st.markdown("<h1 style='text-align: center;'>User Guide: Need Statements</h1>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center;'>Create a Need Statement</h3>", unsafe_allow_html=True)
+    # add in a description of this feature
+
+    st.markdown("<h3 style='text-align: center;'>Scope a Need Statement</h3>", unsafe_allow_html=True)
+    # add in a description of this feature
+
+    st.markdown("<h3 style='text-align: center;'>Create Need Statement Variants</h3>", unsafe_allow_html=True)
+    # add in a description of this feature
+
+
+
     
 main()
 
