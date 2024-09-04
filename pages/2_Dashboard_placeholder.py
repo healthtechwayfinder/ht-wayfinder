5bcdaad81da0c3967c638ac017c2d22e179e72cf
AnaTraperoM <60890310+AnaTraperoM@users.noreply.github.com>
2024-09-04 18:54:23 +0100

    Update 2_Dashboard_placeholder.py

--- pages/2_Dashboard_placeholder.py
+++ pages/2_Dashboard_placeholder.py
@@ -73,7 +73,6 @@ import os
 
 st.markdown("<h3 style='text-align: center;'>What would you like to do?</h3>", unsafe_allow_html=True)
 
-st.experimental_rerun()  # Forces a rerun of the app, ensuring fresh state
 
 
 # # def main():
