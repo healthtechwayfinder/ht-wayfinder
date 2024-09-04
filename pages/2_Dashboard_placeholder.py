7ac94eae50dc3147de0e785da4e24308f9b0f34c
AnaTraperoM <60890310+AnaTraperoM@users.noreply.github.com>
2024-09-04 18:35:38 +0100

    Update 2_Dashboard_placeholder.py

--- pages/2_Dashboard_placeholder.py
+++ pages/2_Dashboard_placeholder.py
@@ -27,7 +27,7 @@ st.markdown(
 # Your logo URL
 logo_url = "https://raw.githubusercontent.com/Aks-Dmv/bio-design-hms/main/Logo-HealthTech.png"  # Replace with the actual URL of your logo
 
-Display the title with the logo below it
+# Display the title with the logo below it
 # st.markdown(
 #     f"""
 #     <div style="text-align: center;">
