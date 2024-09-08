960418dcdb7a7efc8416137a68ba2085824a1ab7
swamplet <bridget.slomka@gmail.com>
2024-09-08 04:17:01 -0400

    Update test_page.py

--- pages/test_page.py
+++ pages/test_page.py
@@ -1,62 +1,15 @@
-import pandas as pd
 import streamlit as st
-from streamlit_gsheets import GSheetsConnection
 
-# Establish a connection to the Google sheets spreadsheet.
-conn = st.connection("gsheets", type=GSheetsConnection)
+def clear_text_input():
+    st.session_state.text_input = ''
 
-# Fetch Existing fields
-existing_data = conn.read(worksheet="Waste", usecols=list(range(13)), ttl=5)
-existing_data = existing_data.dropna(how="all")
+if 'text_input' not in st.session_state:
+    st.session_state.text_input = ''
 
-# Data Entry Form:
-with st.form(key="waste_form"):
-    facility_manager_name = st.text_input(label="What is your Name and Surname? *")
-    mixed_waste = st.text_input(label="How much mixed waste[kg] have you disposed of?")
-    organic_or_wet_waste = st.text_input(label="How much organic/Wet waste[kg] have you disposed of?")
+text_input = st.text_input("Your message here", value=st.session_state.text_input, key="text_input")
 
-    # Mark mandatory fields
-    st.markdown("**required*")
-    submit_button = st.form_submit_button(label="Submit waste disposal details")
+if st.button("Submit"):
+    # Do something with the text input
+    clear_text_input()  # Clear the text input after form submission
 
-    if submit_button:
-        # check if all mandatory fields have been filled
-        if not facility_manager_name or not mixed_waste or not organic_or_wet_waste:
-            st.warning("Please ensure that all the mandatory fields have been filled out")
-            st.stop()
-        else:
-            # add new row with filled data:
-            waste_data = pd.DataFrame(
-                [
-                    {   "Manager Name": facility_manager_name,
-                        "Mixed waste": mixed_waste,
-                        "Organic/Wet waste": organic_or_wet_waste,
-                    }
-                ]
-            )
-
-            updated_df = pd.concat([existing_data, waste_data], ignore_index=True)
-
-            # Update google sheet
-            conn.update(worksheet="Waste", data=updated_df)
-
-
-
-
-
-# import streamlit as st
-
-# with st.form(key="my_form"):
-#     text_input = st.text_input(label="Need Statement:")
-#     submit_button = st.form_submit_button(label="Submit")
-
-#     if submit_button:
-#         if text_input:
-#             need_statement = text_input
-#             st.write("Need statement recorded!")
-#             # st.write(f'You entered: {need_statement}')
-
-
-#         else:
-#             st.warning("Please enter a need statement!")
 
