f4782486b5d6aa97ad8fbe6bbe106177f3f1bd58
swamplet <bridget.slomka@gmail.com>
2024-09-08 04:18:16 -0400

    Update test_page.py

--- pages/test_page.py
+++ pages/test_page.py
@@ -1,15 +1,27 @@
 import streamlit as st
 
-def clear_text_input():
-    st.session_state.text_input = ''
+# Initialize the session state for the text inputs
+if 'input1' not in st.session_state:
+    st.session_state.input1 = ''
+if 'input2' not in st.session_state:
+    st.session_state.input2 = ''
 
-if 'text_input' not in st.session_state:
-    st.session_state.text_input = ''
+# Function to clear inputs
+def clear_form():
+    st.session_state.input1 = ''
+    st.session_state.input2 = ''
 
-text_input = st.text_input("Your message here", value=st.session_state.text_input, key="text_input")
-
-if st.button("Submit"):
-    # Do something with the text input
-    clear_text_input()  # Clear the text input after form submission
-
-
+# Create a form
+with st.form("my_form"):
+    input1 = st.text_input("Enter text 1", value=st.session_state.input1, key='input1')
+    input2 = st.text_input("Enter text 2", value=st.session_state.input2, key='input2')
+    
+    # Form submit button
+    submitted = st.form_submit_button("Submit")
+    
+    if submitted:
+        st.write("Text 1:", input1)
+        st.write("Text 2:", input2)
+        
+        # Clear the inputs after submission
+        clear_form()
