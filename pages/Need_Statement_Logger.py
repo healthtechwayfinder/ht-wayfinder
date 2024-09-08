30c69b6dab9aadde9136747fa1d78f28acac7fcb
swamplet <bridget.slomka@gmail.com>
2024-09-08 13:58:14 -0400

    Update Need_Statement_Logger.py

--- pages/Need_Statement_Logger.py
+++ pages/Need_Statement_Logger.py
@@ -385,11 +385,41 @@ st.markdown("<h4 style='font-size:20px;'>Need Statement:</h4>", unsafe_allow_htm
 # need Text Area
 #st.session_state['need_statement'] = st.text_area("need:", value=st.session_state["need_statement"], height=100)
 
+# Function to clear form inputs
+# def clear_form():
+#     st.session_state.input1 = ''
+
+# Function to handle form submission
+def submit_form():
+    # Form submission logic
+    if need_input:
+            st.session_state["need_statement"] = st.session_state["need_input"]
+
+           # need_statement = need_input
+            problem = problem_input
+            population = population_input
+            outcome = outcome_input
+            notes = notes_input
+            # update_need_ID()
+            st.write("Need statement recorded!")
+            # st.write(f'Relevant Observations: {observation_ID}')
+            # st.write(f'Need ID: {st.session_state['need_ID']}')
+            # st.write(f'Problem: {problem}')
+            # st.write(f'Population: {population}')
+            # st.write(f'Outcome: {outcome}')
+            # st.write(f'Notes: {notes}')
+            recordNeed(st.session_state['need_ID'], st.session_state['need_date'], need_statement, problem, population, outcome, observation_ID, notes)
+    clear_need()
+
+
+
 with st.form(key="my_form"):
     need_input = st.text_input(label="There is a need for...")
     notes_input = st.text_input(label="Relevant Notes:")
     #notes_input = st.text_area("Relevant Notes:", value=st.session_state["notes_input"], height=100)
     submit_button = st.form_submit_button(label="Submit")
+    submitted = st.form_submit_button("Submit", on_click=submit_form)
+
     # st.button("Clear need", on_click=clear_text)
 
     # Button to Clear the need Text Area
@@ -400,7 +430,9 @@ with st.form(key="my_form"):
 
     if submit_button:
         if need_input:
-            need_statement = need_input
+            st.session_state["need_statement"] = st.session_state["need_input"]
+
+            # need_statement = need_input
             problem = problem_input
             population = population_input
             outcome = outcome_input
