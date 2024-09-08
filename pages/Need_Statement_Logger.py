51146de32ecd66050f0c08028a6376cdb0c8298e
swamplet <bridget.slomka@gmail.com>
2024-09-08 01:05:10 -0400

    Update Need_Statement_Logger.py

--- pages/Need_Statement_Logger.py
+++ pages/Need_Statement_Logger.py
@@ -204,7 +204,7 @@ def update_need_ID():
         ]
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
     client = gspread.authorize(creds)
-    need_sheet = client.open("BioDesign Observation Record").worksheet('Need_Log')
+    need_sheet = client.open("BioDesign Observation Record").sheet1
     column_values = need_sheet.col_values(1) 
 
     # find all need ids with the same date
