2fead670d15b96d5fa41edf94c6473dc6158d52f
swamplet <bridget.slomka@gmail.com>
2024-09-08 13:44:37 -0400

    Update Need_Statement_Logger.py

--- pages/Need_Statement_Logger.py
+++ pages/Need_Statement_Logger.py
@@ -180,7 +180,7 @@ def recordNeed(need_ID, need_date, need_statement, problem, population, outcome,
 
 # reset textboxes, except for the date
 def clear_need():
-    st.session_state.need_statement = ""
+    st.session_state.need_statement = ''
     #st.session_state.input1 = ''
 
     # if 'need_summary' in st.session_state:
