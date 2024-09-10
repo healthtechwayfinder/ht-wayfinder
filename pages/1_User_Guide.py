464ebbcdea3a5b0e9942c3c8fda67dad69eb93b7
swamplet <bridget.slomka@gmail.com>
2024-09-10 00:59:00 -0400

    Update 1_User_Guide.py

--- pages/1_User_Guide.py
+++ pages/1_User_Guide.py
@@ -2,30 +2,62 @@ import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 
 
+
 def main():
-    st.markdown("<h1 style='text-align: center;'>User Guide: Observations</h1>", unsafe_allow_html=True)
-    st.markdown("<h3 style='text-align: center;'>Add an Observation</h3>", unsafe_allow_html=True)
-    # add in a description of this feature
-    
-    st.markdown("<h3 style='text-align: center;'>Chat with Observations</h3>", unsafe_allow_html=True)
-    # add in a description of this feature
+    st.markdown("<h1 style='text-align: center;'>User Guide:</h1>", unsafe_allow_html=True)
+    # Create a button using Streamlit's native functionality
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.write("Follow this link for documentationa and examples for each of Wayfinder's features.")
 
-    st.markdown("<h3 style='text-align: center;'>View Observations</h3>", unsafe_allow_html=True)
-    # add in a description of this feature
+    st.markdown("<br>", unsafe_allow_html=True)
 
     
-    st.markdown("<h3 style='text-align: center;'>Glossary</h3>", unsafe_allow_html=True)
-    # add in a description of this feature
+    st.markdown("""
+    <style>
+    div.stButton > button {
+        background-color: #A51C30;
+        color: white;
+        font-size: 16px;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 5px;
+    }
+    div.stButton > button:hover {
+        background-color: #E7485F;
+        color: white;
+    }
+    </style>
+    """, unsafe_allow_html=True)
 
-    st.markdown("<h1 style='text-align: center;'>User Guide: Need Statements</h1>", unsafe_allow_html=True)
-    st.markdown("<h3 style='text-align: center;'>Create a Need Statement</h3>", unsafe_allow_html=True)
-    # add in a description of this feature
 
-    st.markdown("<h3 style='text-align: center;'>Scope a Need Statement</h3>", unsafe_allow_html=True)
-    # add in a description of this feature
+    if st.button('Healthtech Wayfinder User Guide'):
+        st.write('Documentation Coming Soon!')
+    #st.link_button("Healthtech Wayfinder User Guide", "https://streamlit.io/gallery")
 
-    st.markdown("<h3 style='text-align: center;'>Create Need Statement Variants</h3>", unsafe_allow_html=True)
-    # add in a description of this feature
+
+    
+    # st.markdown("<h3 style='text-align: center;'>Add an Observation</h3>", unsafe_allow_html=True)
+    # # add in a description of this feature
+    
+    # st.markdown("<h3 style='text-align: center;'>Chat with Observations</h3>", unsafe_allow_html=True)
+    # # add in a description of this feature
+
+    # st.markdown("<h3 style='text-align: center;'>View Observations</h3>", unsafe_allow_html=True)
+    # # add in a description of this feature
+
+    
+    # st.markdown("<h3 style='text-align: center;'>Glossary</h3>", unsafe_allow_html=True)
+    # # add in a description of this feature
+
+    # st.markdown("<h1 style='text-align: center;'>User Guide: Need Statements</h1>", unsafe_allow_html=True)
+    # st.markdown("<h3 style='text-align: center;'>Create a Need Statement</h3>", unsafe_allow_html=True)
+    # # add in a description of this feature
+
+    # st.markdown("<h3 style='text-align: center;'>Scope a Need Statement</h3>", unsafe_allow_html=True)
+    # # add in a description of this feature
+
+    # st.markdown("<h3 style='text-align: center;'>Create Need Statement Variants</h3>", unsafe_allow_html=True)
+    # # add in a description of this feature
 
 
 
