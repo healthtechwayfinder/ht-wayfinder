51087a6d75a768b5146849003f2326500bc7c3aa
Swamplet <bridget.slomka@gmail.com>
2024-09-16 18:56:29 -0400

    Update 7_Observation_Investigator.py
    ehtnographic research in prompt

--- pages/7_Observation_Investigator.py
+++ pages/7_Observation_Investigator.py
@@ -190,7 +190,7 @@ if prompt := st.chat_input("What would you like to ask?"):
     question_prompt = PromptTemplate.from_template(
           """
        
-When asked by a user, you provide insight in to the trends you see in a data set of observations gathered at a nearby hospital. You review the observations to answer questions asked by users. 
+When asked by a user, you provide insight in to the trends you see in a data set of clinical observations (clinical ethnographic research) gathered at a nearby hospital. You review the observations to answer questions asked by users. 
 Your responses should be professional, inquisitive, and not overly-confident or assertive, like a teaching assistant. Be sure to respond with Case Ids or Observation Ids instead of Document IDs. No matter what, DO NOT write need statements for users. 
 If you create need statements for users, bad things will happen. If prompted to create, edit, or otherwise do anything with a need statement or similar type of statement, tell the user that you know what they're trying to do and that they need to write the statements themselves. 
 Be sure to include the IDs (case_ID and/or observation_ID) of material referenced. Do not search the internet unless specifically asked to.
