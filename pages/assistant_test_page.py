e1b0afe41a07b15ccd7137e78b3f05ed136dc75e
Swamplet <bridget.slomka@gmail.com>
2024-09-16 16:48:27 -0400

    Update assistant_test_page.py

--- pages/assistant_test_page.py
+++ pages/assistant_test_page.py
@@ -100,9 +100,42 @@ st.markdown("""
 # output = llm.invoke({"content": "What's 10 - 4 raised to the 2.7"})
 # output
 
-agent = OpenAIAssistantRunnable(client=OpenAI(api_key=OPENAI_API_KEY), assistant_id=assistant_id, as_agent=True)
+# agent = OpenAIAssistantRunnable(client=OpenAI(api_key=OPENAI_API_KEY), assistant_id=assistant_id, as_agent=True)
 
 
+
+# from openai import OpenAI
+client = OpenAI()
+
+assistant = client.beta.assistants.create(
+  name="Math Tutor",
+  instructions="You are a personal math tutor. Write and run code to answer math questions.",
+  tools=[{"type": "code_interpreter"}],
+  model="gpt-4o",
+)
+
+thread = client.beta.threads.create()
+
+message = client.beta.threads.messages.create(
+  thread_id=thread.id,
+  role="user",
+  content="I need to solve the equation `3x + 11 = 14`. Can you help me?"
+)
+
+
+run = client.beta.threads.runs.create_and_poll(
+  thread_id=thread.id,
+  assistant_id=assistant_ID,
+  instructions="Please address the user as Jane Doe. The user has a premium account."
+)
+
+if run.status == 'completed': 
+  messages = client.beta.threads.messages.list(
+    thread_id=thread.id
+  )
+  print(messages)
+else:
+  print(run.status)
 
 
 
