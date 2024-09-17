5ef7e4ab8968c9e80e5a352dc08c757a7d071efc
AnaTraperoM <60890310+AnaTraperoM@users.noreply.github.com>
2024-09-17 19:02:22 -0400

    Update 3_Case_Logger.py

--- pages/3_Case_Logger.py
+++ pages/3_Case_Logger.py
@@ -618,7 +618,7 @@ if action == "Add New Case":
             editable_fields[key] = st.text_input(f"{key}", value=value)
 
             # Process tags when the key is 'Tags'
-            key.strip().lower() == 'tags':
+            if key.strip().lower() == 'tags':
                 st.write(f"Processing line: key='{key}', value='{value}'")
                 tags_values = [tag.strip() for tag in value.split(",")]
                 st.write(f"Tags after splitting and stripping: {tags_values}")
