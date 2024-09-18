fbfd20b838e1f8ff697ecb36242304a301000911
AnaTraperoM <60890310+AnaTraperoM@users.noreply.github.com>
2024-09-18 13:33:38 -0400

    Update 3_Case_Logger.py

--- pages/3_Case_Logger.py
+++ pages/3_Case_Logger.py
@@ -254,14 +254,14 @@ if action == "Add New Case":
     #         st.session_state['result'] = extractCaseFeatures(st.session_state['case_description'])
 
     with col1:
-    if st.button("Submit Case"):
-        st.session_state['case_title'] = generateCaseSummary(st.session_state['case_description'])
-        if st.session_state['case_title']:
-            with st.container():
-                st.text_area("Case Title (editable):", value=st.session_state['case_title'], height=100, key="case_title_edit")
-        st.session_state['result'] = extractCaseFeatures(st.session_state['case_description'])
-
+        if st.button("Submit Case"):
+            st.session_state['case_title'] = generateCaseSummary(st.session_state['case_description'])
+            if st.session_state['case_title']:
+                with st.container():
+                    st.text_area("Case Title (editable):", value=st.session_state['case_title'], height=100, key="case_title_edit")
+            st.session_state['result'] = extractCaseFeatures(st.session_state['case_description'])
     
+        
     parsed_result = st.session_state['result']
     tags_values = []
 
