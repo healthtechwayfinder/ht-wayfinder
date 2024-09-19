429069142a123613ade5e5370ba84e4a7b8f941a
AnaTraperoM <60890310+AnaTraperoM@users.noreply.github.com>
2024-09-19 11:34:56 -0400

    Update 3_Case_Logger.py

--- pages/3_Case_Logger.py
+++ pages/3_Case_Logger.py
@@ -567,6 +567,7 @@ if action == "Add New Case":
             if status:
                 st.session_state['result'] = "Case added to your team's database."
                 st.session_state['rerun'] = True
+                clear_case()
                 st.rerun() 
             else:
                 st.session_state['result'] = "Error adding case to your team's database. Please try again!"
