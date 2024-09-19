d4228ec030ef6c90ec03a5d6a956464fde994f2c
Swamplet <bridget.slomka@gmail.com>
2024-09-19 11:57:28 -0400

    Update 4_Observation_Logger.py

--- pages/4_Observation_Logger.py
+++ pages/4_Observation_Logger.py
@@ -528,6 +528,7 @@ if st.session_state['observation_summary'] != "":
 
 # here, add the function call to turn parsed results into editable text fields  
 
+parsed_observation = st.session_state['parsed_observation']
 
  # Ensure 'result' exists
     if isinstance(parsed_observation, dict):
