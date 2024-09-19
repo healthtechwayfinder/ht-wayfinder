9e824335b29d4f78b1dbcda35fdc79e187245e1f
Swamplet <bridget.slomka@gmail.com>
2024-09-19 14:32:04 -0400

    Update draft_view_logs.py

--- pages/draft_view_logs.py
+++ pages/draft_view_logs.py
@@ -57,7 +57,7 @@ with col1:
     # Create a dropdown for selecting which sheet to view
     st.markdown("##### Sheet")
 
-    selected_sheet = st.selectbox("##### Sheet",list(worksheets.keys()))
+    selected_sheet = st.selectbox("View...",list(worksheets.keys()))
 
 # Fetch data from Google Sheets based on the selected worksheet
 def load_data(worksheet_name):
