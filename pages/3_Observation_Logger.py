41094ffb3be5a739ea410cabf22421f93df73ec2
AnaTraperoM <60890310+AnaTraperoM@users.noreply.github.com>
2024-09-20 13:19:59 -0400

    Update 3_Observation_Logger.py

--- pages/3_Observation_Logger.py
+++ pages/3_Observation_Logger.py
@@ -634,6 +634,9 @@ def update_observation(observation_id, updated_data):
         # Case ID not found
         print(f"observation ID {observation_id} not found")
         return False
+    except Exception as e:
+        print(f"Error")
+        return []
 
 
 # If the user chooses "Add New Case"
