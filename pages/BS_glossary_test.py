e183da473129a188e745823f19b37ee0de05af2a
Swamplet <bridget.slomka@gmail.com>
2024-09-20 00:25:20 -0400

    Update BS_glossary_test.py

--- pages/BS_glossary_test.py
+++ pages/BS_glossary_test.py
@@ -47,8 +47,8 @@ def clear_row_by_term(term_to_search, glossary_sheet):
         num_cols = glossary_sheet.col_count
         
         # Clear the contents of the row by replacing it with empty strings
-        # glossary_sheet.update(f'A{row_index}:Z{row_index}', [[''] * num_cols])
-        glossary_sheet.delete_row(row_index)
+        glossary_sheet.update(f'A{row_index}:Z{row_index}', [[''] * num_cols])
+        # glossary_sheet.delete_row(row_index)
         
         print(f"Cleared contents of row {row_index} for term '{term_to_search}'")
         
