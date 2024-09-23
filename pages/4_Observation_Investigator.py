497d455ca089d85f6b413c0c4d1d6c9227d63242
Akshay Dharmavaram <akshaydharmavaram@gmail.com>
2024-09-23 17:40:41 -0400

    bug

--- pages/4_Observation_Investigator.py
+++ pages/4_Observation_Investigator.py
@@ -8,7 +8,7 @@ from utils.google_sheet_utils import create_new_chat_sheet, get_case_description
 from utils.llm_utils import create_llm, get_prompt
 from utils.page_formatting import add_investigator_formatting
 from utils.initialize_session import initialize_investigator_session
-from utils.chatbot_utils import fetch_similar_data, invoke_chain, update_session
+from utils.chatbot_utils import fetch_similar_data, update_session
 
 check_if_already_logged_in()
 add_investigator_formatting()
@@ -19,7 +19,7 @@ observation_chat_chain = get_prompt() | llm | StrOutputParser()
 
 # Handle new input
 if prompt := st.chat_input("What would you like to ask?"):
-    
+
     with get_openai_callback() as cb:
         new_output = observation_chat_chain.invoke(fetch_similar_data(prompt),)
 
