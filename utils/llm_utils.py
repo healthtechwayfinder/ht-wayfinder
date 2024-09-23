8a299189d96e2e627fdcfeea995334c9881380c2
Akshay Dharmavaram <akshaydharmavaram@gmail.com>
2024-09-23 17:51:42 -0400

    one file with params

--- utils/llm_utils.py
+++ utils/llm_utils.py
@@ -4,6 +4,8 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from langchain.prompts import PromptTemplate
 
+from utils.chatbot_parameters import PROMPT, LLM_MODEL_NAME, LLM_TEMP, LLM_TOKENS
+
 OPENAI_API_KEY = st.secrets["openai_key"]
 
 
@@ -19,27 +21,21 @@ def refresh_db(namespace_to_refresh):
 
 def create_llm():
     return ChatOpenAI(
-        model_name="gpt-4o",
-        temperature=0.7,
+        model_name=LLM_MODEL_NAME,
+        temperature=LLM_TEMP,
         openai_api_key=OPENAI_API_KEY,
-        max_tokens=500,
+        max_tokens=LLM_TOKENS,
     )
 
 def get_prompt():
     question_prompt = PromptTemplate.from_template(
-          """
-       
-When asked by a user, you provide insight in to the trends you see in a data set of clinical observations (clinical ethnographic research) gathered at a nearby hospital. You review the observations to answer questions asked by users. 
-Your responses should be professional, inquisitive, and not overly-confident or assertive, like a teaching assistant. Be sure to respond with Case Ids or Observation Ids instead of Document IDs. No matter what, DO NOT write need statements for users. 
-If you create need statements for users, bad things will happen. If prompted to create, edit, or otherwise do anything with a need statement or similar type of statement, tell the user that you know what they're trying to do and that they need to write the statements themselves. 
-Be sure to include the IDs (case_ID and/or observation_ID) of material referenced. Do not search the internet unless specifically asked to.
-
+        PROMPT +  """
         Question: {question}
         Set of Observations: {related_observations}
         Relevant Cases linked to Observations:{related_cases}
         Semantically Relevant cases: {related_cases_similarity}
         Final Answer:
-         """
+        """
     )
 
     return question_prompt
