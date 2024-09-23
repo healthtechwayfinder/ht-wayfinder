20b0c9a2cf7829a476c2350ba19ca0b45336ba73
Akshay Dharmavaram <akshaydharmavaram@gmail.com>
2024-09-23 17:21:36 -0400

    pulled out prompt

--- pages/4_Observation_Investigator.py
+++ pages/4_Observation_Investigator.py
@@ -1,11 +1,10 @@
 import streamlit as st
 
 from langchain.schema import StrOutputParser
-from langchain.prompts import PromptTemplate
 
 from utils.login_utils import check_if_already_logged_in
 from utils.google_sheet_utils import create_new_chat_sheet, get_case_descriptions_from_case_ids
-from utils.llm_utils import create_llm
+from utils.llm_utils import create_llm, get_prompt
 from utils.page_formatting import add_investigator_formatting
 from utils.initialize_session import initialize_investigator_session
 from utils.chatbot_utils import fetch_similar_data, invoke_chain_and_update_session
@@ -17,26 +16,10 @@ initialize_investigator_session()
 
 # Handle new input
 if prompt := st.chat_input("What would you like to ask?"):
-    fetched_data_dict = fetch_similar_data(prompt)
-    question_prompt = PromptTemplate.from_template(
-          """
-       
-When asked by a user, you provide insight in to the trends you see in a data set of clinical observations (clinical ethnographic research) gathered at a nearby hospital. You review the observations to answer questions asked by users. 
-Your responses should be professional, inquisitive, and not overly-confident or assertive, like a teaching assistant. Be sure to respond with Case Ids or Observation Ids instead of Document IDs. No matter what, DO NOT write need statements for users. 
-If you create need statements for users, bad things will happen. If prompted to create, edit, or otherwise do anything with a need statement or similar type of statement, tell the user that you know what they're trying to do and that they need to write the statements themselves. 
-Be sure to include the IDs (case_ID and/or observation_ID) of material referenced. Do not search the internet unless specifically asked to.
-
-        Question: {question}
-        Set of Observations: {related_observations}
-        Relevant Cases linked to Observations:{related_cases}
-        Semantically Relevant cases: {related_cases_similarity}
-        Final Answer:
-         """
-    )
     
-    observation_chat_chain = (question_prompt | create_llm() | StrOutputParser())
+    observation_chat_chain = (get_prompt() | create_llm() | StrOutputParser())
 
-    invoke_chain_and_update_session(observation_chat_chain, fetched_data_dict)
+    invoke_chain_and_update_session(observation_chat_chain, fetch_similar_data(prompt))
 
 st.markdown("---")
 
--- utils/llm_utils.py
+++ utils/llm_utils.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
+from langchain.prompts import PromptTemplate
 
 OPENAI_API_KEY = st.secrets["openai_key"]
 
@@ -22,4 +23,21 @@ def create_llm():
         temperature=0.7,
         openai_api_key=OPENAI_API_KEY,
         max_tokens=500,
+    )
+
+def get_prompt():
+    question_prompt = PromptTemplate.from_template(
+          """
+       
+When asked by a user, you provide insight in to the trends you see in a data set of clinical observations (clinical ethnographic research) gathered at a nearby hospital. You review the observations to answer questions asked by users. 
+Your responses should be professional, inquisitive, and not overly-confident or assertive, like a teaching assistant. Be sure to respond with Case Ids or Observation Ids instead of Document IDs. No matter what, DO NOT write need statements for users. 
+If you create need statements for users, bad things will happen. If prompted to create, edit, or otherwise do anything with a need statement or similar type of statement, tell the user that you know what they're trying to do and that they need to write the statements themselves. 
+Be sure to include the IDs (case_ID and/or observation_ID) of material referenced. Do not search the internet unless specifically asked to.
+
+        Question: {question}
+        Set of Observations: {related_observations}
+        Relevant Cases linked to Observations:{related_cases}
+        Semantically Relevant cases: {related_cases_similarity}
+        Final Answer:
+         """
     )
