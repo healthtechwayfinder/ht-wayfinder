591f3ef17bec03dcd0b2bfeda7c974c5ce707676
AnaTraperoM <60890310+AnaTraperoM@users.noreply.github.com>
2024-09-24 12:37:50 -0400

    Update 3_Observation_Logger.py

--- pages/3_Observation_Logger.py
+++ pages/3_Observation_Logger.py
@@ -986,7 +986,7 @@ elif action == "Edit Existing Observation":
                     observation_stakeholders_string = ", ".join(observation_stakeholders)
                     old_case_id = observation_details.get("Related Case ID", "") 
 
-                    st.write(observation_to_edit, observation_id)
+                    st.write(observation_to_edit, st.session_state['observation_id'])
     
                     updated_data = {
                             "Observation ID": observation_id,
