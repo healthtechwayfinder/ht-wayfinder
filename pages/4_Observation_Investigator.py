e1bcb36e21ea42f49cfeaf0d78a3fdc39b583f18
Helathtech Wayfinder <healthtechwayfinder@gmail.com>
2024-11-11 22:17:26 -0500

    Update 4_Observation_Investigator.py

--- pages/4_Observation_Investigator.py
+++ pages/4_Observation_Investigator.py
@@ -1,29 +1,37 @@
 import streamlit as st
 
-from langchain.schema import StrOutputParser
 from langchain.callbacks import get_openai_callback
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage 
 
 from utils.login_utils import check_if_already_logged_in
-from utils.google_sheet_utils import create_new_chat_sheet, get_case_descriptions_from_case_ids
-from utils.llm_utils import create_llm, get_prompt
 from utils.page_formatting import add_investigator_formatting
 from utils.initialize_session import initialize_investigator_session
-from utils.chatbot_utils import fetch_similar_data, update_session
+from utils.chatbot_utils import update_session, get_chat_response
 
 check_if_already_logged_in()
 add_investigator_formatting()
 initialize_investigator_session()
 
-llm = create_llm()
-observation_chat_chain = get_prompt() | llm | StrOutputParser()
-
 # Handle new input
-if prompt := st.chat_input("What would you like to ask?"):
+if user_input := st.chat_input("What would you like to ask ?"):
 
-    with get_openai_callback() as cb:
-        new_output = observation_chat_chain.invoke(fetch_similar_data(prompt),)
+    # show the user input
+    with st.chat_message("user"):
+        st.markdown(user_input)
 
-    update_session(new_output)
+    # create the chat messages for the llm
+    if user_input is not None and user_input.strip()!="":
+        # ai_response = st.write_stream(get_chat_response(user_input))
+        ai_response = get_chat_response(user_input)
+
+    # show the ai response
+    with st.chat_message("assistant"):
+        st.markdown(ai_response)
+
+    st.session_state.messages.append(HumanMessage(content=user_input))
+    st.session_state.messages.append(AIMessage(content=ai_response))
+
+    update_session(ai_response)
 
 st.markdown("---")
 
