0fff15f8c2d069e16f707b7b0ab32b01e58b6967
Helathtech Wayfinder <healthtechwayfinder@gmail.com>
2024-11-11 22:36:07 -0500

    Update google_sheet_utils.py

--- utils/google_sheet_utils.py
+++ utils/google_sheet_utils.py
@@ -3,6 +3,12 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
 
+from langchain_pinecone import PineconeVectorStore
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+
+from langchain_core.documents import Document
+
+
 # Access the credentials from Streamlit secrets
 creds_dict = {
     "type" : st.secrets["gwf_service_account"]["type"],
@@ -42,8 +48,13 @@ def create_new_chat_sheet():
 #     return data
 
 def get_case_sheet_as_dict():
-    case_sheet = CLIENT.open("2024 Healthtech Identify Log").worksheet("Case Log")
+    case_sheet = CLIENT.open("Copy of 2024 Healthtech Identify Log").worksheet("Case Log")
     data = case_sheet.get_all_records()
+    return data
+
+def get_observation_sheet_as_dict():
+    observation_sheet = CLIENT.open(st.session_state.observation_sheet_name).worksheet("Observation Log")
+    data = observation_sheet.get_all_records()
     return data
 
 def get_case_descriptions_from_case_ids(case_ids):
@@ -54,3 +65,68 @@ def get_case_descriptions_from_case_ids(case_ids):
         for case in data
         if case['Case ID'] in case_ids
     }
+
+def get_observation_descriptions_from_observation_ids(observation_ids):
+    data = get_observation_sheet_as_dict()
+
+    return {
+        observation['Observation ID']: observation['Observation Description']
+        for observation in data
+        if observation['Observation ID'] in observation_ids
+    }
+
+def cases_related_to_observations(list_of_observations_pinecone):
+    case_ids = []
+    for observation in list_of_observations_pinecone:
+        case_ids.append(observation.metadata['Case ID'])
+    return get_case_descriptions_from_case_ids(case_ids)
+
+def observations_related_to_cases(list_of_cases_pinecone):
+    observation_ids = []
+    for case in list_of_cases_pinecone:
+        observation_ids.append(case.metadata['Observation ID'])
+    return get_observation_descriptions_from_observation_ids(observation_ids)
+
+
+def sync_with_pinecone():
+    """Sync the Google Sheet data with Pinecone"""
+    # start_time = datetime.now()
+
+    observations_in_sheet = get_observation_sheet_as_dict()
+
+    # Create a Pinecone vector store
+    observations_db = PineconeVectorStore(
+        index_name=st.secrets["pinecone-keys"]["index_to_connect"],
+        namespace=st.session_state.observation_namespace,
+        embedding=OpenAIEmbeddings(api_key=st.secrets["openai_key"]),
+        pinecone_api_key=st.secrets["pinecone-keys"]["api_key"],
+    )
+
+    observation_ids = [observation['Observation ID'] for observation in observations_in_sheet]
+    observation_descriptions = [observation['Observation Description'] for observation in observations_in_sheet]
+    observation_metadatas = [{k: v for k, v in observation.items() if k not in ['Observation Description']} for observation in observations_in_sheet]
+
+    observations_added = observations_db.add_texts(observation_descriptions, metadatas=observation_metadatas, ids=observation_ids)
+
+    # cases_in_sheet = get_case_sheet_as_dict()
+
+    # cases_db = PineconeVectorStore(
+    #     index_name=st.secrets["pinecone-keys"]["index_to_connect"],
+    #     namespace=st.session_state.cases_namespace,
+    #     embedding=OpenAIEmbeddings(api_key=st.secrets["openai_key"]),
+    #     pinecone_api_key=st.secrets["pinecone-keys"]["api_key"],
+    # )
+
+    # case_ids = [case['Case ID'] for case in cases_in_sheet]
+    # case_descriptions = [case['Case Description'] for case in cases_in_sheet]
+    # case_metadatas = [{k: v for k, v in case.items() if k not in ['Case ID', 'Case Description']} for case in cases_in_sheet]
+
+    # cases_added = cases_db.add_texts(case_descriptions, metadatas=case_metadatas, ids=case_ids)
+
+    # time_taken = datetime.now() - start_time
+    # time_taken = time_taken.total_seconds()
+    # time_taken = float("{:.2f}".format(time_taken))
+
+    # st.write("Time taken to sync data: ", time_taken, " seconds")
+
+    st.write("Synced data from ", len(observations_added), " observations")
