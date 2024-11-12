d2f4ea8c95b1cc3673a84b728450b85ff2d6929c
Helathtech Wayfinder <healthtechwayfinder@gmail.com>
2024-11-11 22:25:27 -0500

    Update initialize_session.py

--- utils/initialize_session.py
+++ utils/initialize_session.py
@@ -1,5 +1,10 @@
 import streamlit as st
 from utils.google_sheet_utils import create_new_chat_sheet
+from utils.chatbot_parameters import SYSTEM_PROMPT
+
+from utils.google_sheet_utils import sync_with_pinecone
+
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 
 def initialize_investigator_session():
     # # Create a new sheet for the chat thread if not already created
@@ -9,7 +14,18 @@ def initialize_investigator_session():
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
+    if "observation_sheet_name" not in st.session_state:
+        st.session_state.observation_sheet_name = "2024 Healthtech Identify Log"
+       
+    if "observation_namespace" not in st.session_state:
+        st.session_state.observation_namespace = "healthtechwf"
+        sync_with_pinecone()
+
     # Display previous messages
     for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+        if isinstance(message, HumanMessage):
+            with st.chat_message("user"):
+                st.markdown(message.content)
+        elif isinstance(message, AIMessage):
+            with st.chat_message("assistant"):
+                st.markdown(message.content)
